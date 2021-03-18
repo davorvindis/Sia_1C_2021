60f621d970e0bd77ed0061a029eefb9d3d86ce78
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-03-18 02:49:40 -0300

    Update dfs.py
    Bug fix in verify if the node is a win .

--- TP1/algorithms/dfs.py
+++ TP1/algorithms/dfs.py
@@ -4,18 +4,18 @@ def dfs(self, root):
     self.current_node = root
 
     while len(self.nodes_to_visit_queue) != 0:
-
-        print('> ', self.current_node.steps)
+        
         self.current_node = self.nodes_to_visit_queue.pop()
+        
         #Verify if the node is a win
-
-        if self.check_win(self.current_node):
-            print("Win")
+        if self.check_moves(self.current_node):
             return
 
         #if node has not been visited
         if self.current_node not in self.visited_nodes:
+            
             #Add it to the list of visited
             self.visited_nodes.add(self.current_node)
+            
             #Expand actual node
             self.expand_with_depth(self.current_node)
