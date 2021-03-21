d7d97eb624f9b0792b8994bb1b3a180448bceda9
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-03-20 23:08:39 -0300

    Update id_a_star.py
    Update ida*

--- TP1/algorithms/id_a_star.py
+++ TP1/algorithms/id_a_star.py
@@ -1,81 +1,85 @@
 import copy
+from collections import deque
+import heapq
 
-def moves_available(self):
-    """
-    @return array (move, cost)
-        move: available moves in [u,r,d,l]
-        cost:   2 if pushing a box,
-                1 otherwise
-    Checks that moves that involve pushing a block are possible given the
-    placement of walls.
-    """
+def id_a_star(graph, root, heuristic):
 
-
-def add_to_astar_queue(nodes, queue, _node, cost):
-    nodes[hash(_node)] = cost
-    add_last = True
-    for index, node in enumerate(queue):
-        if cost <= node.depth + node.h_cost:
-            queue.insert(index, _node)
-            add_last = False
-            break
-    if add_last:
-        queue.append(_node)
-
-
-def a_star(graph, root, heuristic, limit):
-    graph.nodes_to_visit_queue.append(root)
     visited_nodes = set()
-    root.add_h_cost(heuristic(graph,root))
-    visited_nodes.add(root)
-    nodes = {hash(root): 0}
+    root.add_h_cost(heuristic(graph, root))
+    frontierHeap = [(root.get_Fvalue(), root)]
+    heapq.heapify(frontierHeap)
+    nodosExpandidos = 0
 
-    while graph.nodes_to_visit_queue:
-        graph.current_node = graph.nodes_to_visit_queue.popleft()
-        if graph.current_node.depth + graph.current_node.h_cost > limit:
-            continue
-        if graph.check_win(graph.current_node):
-            print("Los pasos para ganar fueron: " + str(graph.current_node.steps))
-            print("La profundidad fue: " + str(graph.current_node.depth))
-            print("En total se expandieron " + str(len(visited_nodes)) + " nodos")
-            print("En total quedaron " + str(len(graph.nodes_to_visit_queue)) + " nodos frontera")
-            print("win")
-            return
-        for move in graph.DIRECTION:
-            child_node = copy.deepcopy(graph.current_node)  # copio nodo
-            child_node.player_position.move_position(graph.DIRECTION[move])  # lo muevo
-            child_node.add_cost()  # agrego costo
-            if graph.check_if_wall(child_node.player_position):  # chequeo si vale -> no es pared
-                continue
-            if graph.check_if_box(child_node.player_position):  # chequeo si hay caja
-                for box in child_node.boxes_positions:
-                    if box == child_node.player_position:
-                        box.move_position(graph.DIRECTION[move])
-                    if not graph.check_if_wall(box) and not graph.check_if_box(box):  # ACÁ EL MOVIMIENTO VALE
-                        child_node.steps.append(move)  # Agrego el movimiento al nodo
+    while frontierHeap:
+
+        node = heapq.heappop(frontierHeap)
+        node = node[1]
+
+        if node not in visited_nodes:
+            visited_nodes.add(node)
+
+        graph.nodes_to_visit_queue.append(node)
+        limit = node.get_Fvalue()
+        #print('Limit : ', limit)
+
+        while graph.nodes_to_visit_queue:
+
+            graph.current_node = graph.nodes_to_visit_queue.popleft()
+            
+            if graph.check_win(graph.current_node):
+                print("Los pasos para ganar fueron : {0}".format(str(graph.current_node.steps)))
+                print("La profundidad fue          : {0}".format(str(graph.current_node.depth)))
+                print("En total se expandieron     : {0} nodos ".format(nodosExpandidos))
+                print("En total quedaron           : {0} nodos frontera".format(len(graph.nodes_to_visit_queue)))
+                return
+
+            else: #not victory
+
+                nodosExpandidos += 1
+
+                for move in graph.DIRECTION:
+
+                    child_node = copy.deepcopy(graph.current_node)
+                    child_node.player_position.move_position(graph.DIRECTION[move])
+                    child_node.add_cost()
+
+                    if graph.check_if_wall(child_node.player_position): #hit wall
+                        continue
+
+                    if graph.check_if_box(child_node.player_position): #hit box
+                        for box in child_node.boxes_positions:         #which box
+                            if box == child_node.player_position: 
+                                box.move_position(graph.DIRECTION[move]) #move box in the same direction.
+                            if not graph.check_if_wall(box) and not graph.check_if_box(box):
+
+                                child_node.steps.append(move)
+                                heuristic_cost = heuristic(graph, child_node)
+                                child_node.add_h_cost(heuristic_cost)
+                                F_score = child_node.get_Fvalue()
+                                
+                                if child_node not in visited_nodes:
+                                    visited_nodes.add(child_node) # getLevel() ?
+                                    if F_score > limit:
+                                        heapq.heappush(frontierHeap, (F_score, child_node))
+                                    else: 
+                                        graph.nodes_to_visit_queue.append(child_node)
+                            else:
+                                continue
+                        continue
+
+                    else:
+
+                        #not(hit,wall): OK
+                        child_node.steps.append(move)
                         heuristic_cost = heuristic(graph, child_node)
                         child_node.add_h_cost(heuristic_cost)
-                        total_cost = child_node.h_cost + child_node.depth
-                        if child_node not in visited_nodes:  # Me fijo si no lo visite
+                        F_score = child_node.get_Fvalue()
+                        
+                        if child_node not in visited_nodes:
                             visited_nodes.add(child_node)
-                            add_to_astar_queue(nodes, graph.nodes_to_visit_queue, child_node, total_cost)
-                            nodes[hash(child_node)] = total_cost
-                        else:  # si lo visité
-                            if nodes[hash(child_node)] > total_cost:
-                                nodes[hash(child_node)] = total_cost
-                    else:
-                        continue
-                continue
-            else:
-                child_node.steps.append(move)  # Agrego el movimiento al nodo
-                heuristic_cost = heuristic(graph, child_node)
-                child_node.add_h_cost(heuristic_cost)
-                total_cost = child_node.h_cost + child_node.depth
-                if child_node not in visited_nodes:  # Me fijo si no lo visite
-                    visited_nodes.add(child_node)
-                    add_to_astar_queue(nodes, graph.nodes_to_visit_queue, child_node, total_cost)
-                    nodes[hash(child_node)] = total_cost
-                else:  # si lo visité
-                    if nodes[hash(child_node)] > total_cost:
-                        nodes[hash(child_node)] = total_cost
-    print("Solucion no encontrada")
+                            if F_score > limit:
+                                heapq.heappush(frontierHeap, (F_score, child_node))
+                            else: 
+                                graph.nodes_to_visit_queue.append(child_node)
+
+    print('Solucion no encontrada\n')
