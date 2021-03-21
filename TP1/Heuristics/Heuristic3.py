73f424584a14faa5cd957694211ac4df00a939ae
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-03-20 22:34:58 -0300

    Update Heuristic3.py
    Add imports

--- TP1/Heuristics/Heuristic3.py
+++ TP1/Heuristics/Heuristic3.py
@@ -2,6 +2,9 @@
 # distancia del jugador a la caja mas cercana (h2)
 # + la suma de la mínima distancia entre los objetivos y cualquier caja (h1)
 
+from Heuristics.Heuristic1 import heuristic1
+from Heuristics.Heuristic2 import heuristic2
+
 def heuristic3(graph, node):
 
     return heuristic1(graph, node) + heuristic2(graph, node)
